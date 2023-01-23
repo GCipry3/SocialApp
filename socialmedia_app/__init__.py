3e8f1782cc936abf5a29470a78bda3e5951618f8
GCipry3 <galbeazaciprian30@gmail.com>
2023-01-23 21:25:44 +0200

    Added user authentication functionality including register, login, and logout forms and pages. Implemented functionality to handle form submissions , validate user input , and perform password hashing and verification.

--- socialmedia_app/__init__.py
+++ socialmedia_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 # Create the app
 app = Flask(__name__)
@@ -13,6 +13,11 @@ db = SQLAlchemy(app)
 
 # Bcrypt
 bcrypt = Bcrypt(app)
+
+# Login Manager
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 
 # Import the routes
 from socialmedia_app import routes
